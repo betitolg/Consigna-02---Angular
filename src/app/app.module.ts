6e5e6a96463fd2b615d1624031a6bb400c9d0aff
Daniel Lopez <betitolg@hotmail.com>
2022-05-02 21:32:42 -0500

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftmenuComponent } from './leftmenu/leftmenu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftmenuComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
